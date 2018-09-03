63c893e749b1a0749d9397a5c56e5928e41abbfd
LeoEatle <liuyitao811@hotmail.com>
2018-09-03 18:15:17 +0800

    feat: 更新package.json，更新test.js，删除public
    fix: 补充license
    
    feat: 添加travis配置，删除多余代码，增加index转发
    
    fix: 修改nodejs版本
    
    fix: 增强错误处理
    
    fix: 更新travis.yml
    
    fix: 调整测试文件位置，增加loader resolve路径以保证能找到文件
    
    fix: update README
    
    fix: 去除多余的项目依赖
    
    feat: 增加travis徽标

--- test/template-project/src/pages/index/pageComponent.js
+++ test/template-project/src/pages/index/pageComponent.js
@@ -1,10 +1,11 @@
 'use strict'
 import './index.scss'
+import './index.less'
 import React, {
     Component
 } from 'react';
 import 'react-fastclick';
-import Button from 'material-ui/Button';
+import Button from '@material-ui/core/Button';
 
 export default class pageComponent extends Component {
     constructor(props, context) {
